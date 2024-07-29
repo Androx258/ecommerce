99fd775d62e4a564b13be5b5ac977fb9b8bb37c2
Androx258 <m.andresm94@gmail.com>
2024-07-29 16:51:09 -0500

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
+    <title>Ecommerce</title>
+</head>
+
+<body>
+    <main class="main">
+            
+        <div class="container__products">
+            <img src="{{asset('videos/tienda-virtual.png')}}" alt="" class="landing__image">
+            <h1>Hola</h1>
+        </div>
+    </main>
+</body>
+
+</html>
